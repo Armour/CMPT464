233d2ab37d778711586dfe85aaa3caa542e41f16
Armour <497052684@qq.com>
2016-02-19 00:54:58 -0800

    update A1: added loop subdivision

---
+++ Assignment 1/include/lib_consts.h
@@ -0,0 +1,69 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  lib_consts.h
+//
+//  The header file for all global constants
+//
+//  Project         : SmfView
+//  Name            : Chong Guo
+//  Student ID      : 301295753
+//  SFU username    : armourg
+//  Instructor      : Richard Zhang
+//
+//  Created by Armour on 2/11/2016
+//  Copyright (c) 2016 Armour. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef SMFVIEW_LIB_CONSTS_H_
+#define SMFVIEW_LIB_CONSTS_H_
+
+namespace libconsts {
+
+// Windows property
+const int kWindowPositionX = 0;
+const int kWindowPositionY = 0;
+const int kWindowSizeWidth = 1000;
+const int kWindowSizeHeight = 500;
+
+// Max number of vertex, faces and edges
+const int kMaxVertexNum = 200000;
+const int kMaxFaceNum = 400000;
+const int kMaxEdgeNum = 600000;
+
+// Subdivision type
+const int kSubdivisionLoop = 0;
+const int kSubdivisionButterfly = 1;
+
+// Flag indicate input is vertex or face data
+const int kFlagVertex = 0;
+const int kFlagFace = 1;
+
+// Import and export semaphore
+const int kImportLockOff = 0;
+const int kImportLockOn = 1;
+const int kExportLockOff = 0;
+const int kExportLockOn = 1;
+
+// Subdivision semaphore
+const int kSubdivisionLockOff = 0;
+const int kSubdivisionLockOn = 1;
+
+// Import mesh flag
+const int kMeshImportedFalse = 0;
+const int kMeshImportedTrue = 1;
+
+// Mesh type
+const int kMeshTypeFlatShaded = 0;
+const int kMeshTypeSmoothShaded = 1;
+const int kMeshTypeWireFrame = 2;
+const int kMeshTypeShadedEdges = 3;
+
+// Bind buffer element vector length
+const int kElementVecLenPosition = 3;
+const int kElementVecLenColor = 4;
+const int kElementVecLenTotal = 7;
+
+} // namespace libconsts
+
+#endif // SMFVIEW_LIB_CONSTS_H_
---
+++ Assignment 1/include/subdivision.h
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  subdivision.h
+//
+//  The header file for function that do the subdivision
+//
+//  Project         : SmfView
+//  Name            : Chong Guo
+//  Student ID      : 301295753
+//  SFU username    : armourg
+//  Instructor      : Richard Zhang
+//
+//  Created by Armour on 2/18/2016
+//  Copyright (c) 2016 Armour. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef SMFVIEW_SUBDIVISION_H_
+#define SMFVIEW_SUBDIVISION_H_
+
+#include <vector>
+#include <map>
+
+#include "lib_gl.h"
+#include "lib_consts.h"
+#include "winged_edge.h"
+
+using namespace std;
+
+// Some extern variables
+extern int mesh_imported;
+extern int subdivision_semaphore;       // Subdivision semaphore
+extern vector<smfparser::Vertex *> mesh_vertex;     // The vertex data that stored in memory
+extern vector<smfparser::Face *> mesh_faces;        // The faces data that stored in memory
+extern map<pair<int, int>, smfparser::W_edge *> mesh_edges;     // The edges data that stored in memory
+extern map<smfparser::Vertex *, GLuint> vertex_index_map;       // The vertex index map
+extern vector<GLfloat> data_vertex;     // The vertex data used for rendering
+extern vector<GLuint> data_faces;       // The faces data used for rendering
+extern vector<GLuint> data_edges;       // The edges data used for rendering
+extern void UpdateMeshBufferData();
+
+namespace subdivision {
+
+// Update new mesh data from vertex_input and face_input
+void UpdateWingedEdgeMeshData();
+
+// The loop subdivision
+void LoopSubdivision(int level);
+
+// Calculate beta for loop vertex mask with k adjacent vertex
+float LoopVertexMaskBeta(int k);
+
+// The butterfly subdivision
+void ButterflySubdivision(int level);
+
+// Init mesh data for rendering
+void UpdateRenderMeshData();
+
+} // namespace subdivision
+
+#endif // SMFVIEW_SUBDIVISION_H_
